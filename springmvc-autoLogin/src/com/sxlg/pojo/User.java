a92106ad167f76f867c60ddbb952a92832f8a2fe
south-moon-ke <55371859+south-moon-ke@users.noreply.github.com>
2019-09-16 14:02:30 +0800

    上传java项目

---
+++ springmvc-autoLogin/src/com/sxlg/pojo/User.java
@@ -0,0 +1,56 @@
+package com.sxlg.pojo;
+
+import java.util.Date;
+
+public class User {
+	private int id;
+	private String name;
+	private String sex;
+	private Date birthday;
+	private float height;
+	private String username;
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public float getHeight() {
+		return height;
+	}
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
